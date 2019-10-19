0f9b6940eab5375ac4b3281a9096af2f13d61986
drdrenckhahn <darlrdren22@gmail.com>
2019-10-18 20:50:32 -0600

    Got scanning QR codes to work (right now brings up random 3D models)

---
+++ CSC567FinalProject/Assets/Scripts/ArScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ArScript : MonoBehaviour
+{
+    [SerializeField] private Button back_btn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get game objects
+        back_btn = GameObject.Find("back_btn").GetComponent<Button>();
+
+        // add listener for when the back button gets clicked
+        back_btn.onClick.AddListener(delegate
+        {
+            back_btn_clicked(back_btn);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+
+                //quit application on return button
+
+                Application.Quit();
+
+                return;
+
+            }
+        }
+    }
+
+    void back_btn_clicked(Button btn_clicked)
+    {
+        // start scene to go back to main menu
+        SceneManager.LoadScene("MainMenuScene", LoadSceneMode.Single);
+    }
+}
